000a9641fd22aed317f46f9d0e59e8d8ab7cbcbe
Tiffany Huang <tiffany.jenyun.huang@gmail.com>
2020-01-12 17:55:30 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if index.between(0,8) = true && !position_taken(board, index) = true
+  if index.between(0,8) = true && !position_taken?(board, index) = true
     true
   else
     false
