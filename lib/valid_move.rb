09cb150e005de26020ed1cbe38e315393061f176
Tiffany Huang <tiffany.jenyun.huang@gmail.com>
2020-01-12 17:44:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,13 @@
 # code your #valid_move? method here
 
 def valid_move?(board,index)
-  board = 
+  if board >= 0 && board <= 9
+    true
+  else
+    false
+  end
+end
+   
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
